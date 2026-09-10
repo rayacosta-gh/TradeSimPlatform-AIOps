a77631ab5e9394497b34de52770b86a41b00f4af
Ray Acosta <266407091+rayacosta-gh@users.noreply.github.com>
2026-09-10 09:56:38 -0400

    Scaffold Go backend module
    Closes #1

---
+++ cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux() //Go's multiplexer
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	addr := ":8080"
+	log.Printf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
