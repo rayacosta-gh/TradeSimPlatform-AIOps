0711521dc3974751cb57286ec11fe1e59f779e65
rayacosta-gh <266407091+rayacosta-gh@users.noreply.github.com>
2026-09-10 22:26:20 -0400

    Closes #5 (#16)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	mux := http.NewServeMux() //Go's multiplexer
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
 	})
 
 	addr := ":8080"
 	log.Printf("listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := http.ListenAndServe(addr, r); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rayacosta-gh/TradeSimPlatform-AIOps
 
 go 1.26.4
+
+require github.com/go-chi/chi/v5 v5.3.2
